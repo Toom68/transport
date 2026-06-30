b3140b256c6f312ae4bea70e7200c1c83b324fdf
Henis67 <Henis67@users.noreply.github.com>
2026-06-30 23:38:51 +1000

    Symmetric icons on ring, thicken border, hide line through plane

--- flightfocus/src/components/HomeScreen.tsx
+++ flightfocus/src/components/HomeScreen.tsx
@@ -13,10 +13,10 @@ import { MultiplayerModal } from './MultiplayerModal';
 function TransportLogo({ className }: { className?: string }) {
   return (
     <div className={className}>
-      <div className="relative w-full h-full rounded-full border-2 border-theme-accent-border">
-        <Plane className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-7 h-7 text-theme-accent" />
-        <Car className="absolute bottom-0 left-0 translate-x-[-30%] translate-y-1/2 w-6 h-6 text-theme-accent opacity-80" />
-        <Anchor className="absolute bottom-0 right-0 translate-x-[30%] translate-y-1/2 w-6 h-6 text-theme-accent opacity-80" />
+      <div className="relative w-full h-full rounded-full border-4 border-theme-accent-border">
+        <Plane className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-7 h-7 text-theme-accent bg-theme-panel rounded-full p-0.5" />
+        <Car className="absolute bottom-0 left-0 -translate-x-1/2 translate-y-1/2 w-6 h-6 text-theme-accent opacity-80" />
+        <Anchor className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2 w-6 h-6 text-theme-accent opacity-80" />
       </div>
     </div>
   );
